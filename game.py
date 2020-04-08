74886fb6fff61a1332c34b0794f4276b67f5e4b0
Fergus McClean <fergus.mcclean@icloud.com>
2020-04-08 19:38:07 +0100

    renamed widgets, added generate_choices

--- game.py
+++ game.py
@@ -17,33 +17,30 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.mainWidget = QWidget(self)
-        self.questionWidget = QDialog(self)
-        self.questionLayout = QVBoxLayout()
-        self.questionWidget.setLayout(self.questionLayout)
-
-        self.layout = QVBoxLayout()
-        self.answers = QVBoxLayout()
-        self.answersWidget = QWidget()
-        self.answersWidget.setLayout(self.answers)
-
-        self.question = QLabel('')
-        newfont = QFont("Arial", 30, QFont.Bold)
-        self.question.setFont(newfont)
-        self.question.setWordWrap(True)
-        self.question.setMaximumWidth(2000)
-        self.question.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding))
-
-        self.questionLayout.addWidget(self.question)
-
-        self.questionLayout.addWidget(self.answersWidget)
-
-        self.mainWidget.setLayout(self.layout)
+        self.mainLayout = QVBoxLayout()
+        self.mainWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self.mainWidget)
+
+        self.questionLayout = QVBoxLayout()
+        self.questionWidget = QDialog(self)
+        self.questionWidget.setLayout(self.questionLayout)
+        self.questionLabel = QLabel('')
+        self.questionLabel.setFont(QFont("Arial", 30, QFont.Bold))
+        self.questionLabel.setWordWrap(True)
+        self.questionLabel.setMaximumWidth(2000)
+        self.questionLabel.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding))
+        self.questionLayout.addWidget(self.questionLabel)
+        self.mainLayout.addWidget(self.questionWidget)
+
+        self.choicesLayout = QVBoxLayout()
+        self.choicesWidget = QWidget()
+        self.choicesWidget.setLayout(self.choicesLayout)
+        self.questionLayout.addWidget(self.choicesWidget)
+
         self.show()
 
         self.answerWidget = QWidget()
         self.answerLayout = QVBoxLayout()
-
         self.answerWidget.setLayout(self.answerLayout)
 
         self.answerPicture = QLabel()
@@ -53,11 +50,12 @@ class App(QMainWindow):
         self.answer_screen_confirm = QPushButton(text='OK')
         self.answerLayout.addWidget(self.result)
         self.answerLayout.addWidget(self.answer_screen_confirm)
+        # noinspection PyUnresolvedReferences
         self.answer_screen_confirm.clicked.connect(self.ask_question)
 
         self.scoreWidget = QLabel()
         self.scoreWidget.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum))
-        self.layout.addWidget(self.scoreWidget)
+        self.mainLayout.addWidget(self.scoreWidget)
         self.total_correct = 0
         self.total_answered = 0
 
@@ -66,21 +64,27 @@ class App(QMainWindow):
         self.progressWidget.setMaximum(sum([len(drugs[group]) for group in groups]))
         self.progressWidget.setValue(0)
 
-        self.layout.addWidget(self.progressWidget)
+        self.mainLayout.addWidget(self.progressWidget)
 
         self.question_confirm = QPushButton(text='OK')
         self.questionLayout.addWidget(self.question_confirm)
-        self.layout.addWidget(self.questionWidget)
-        self.layout.addWidget(self.answerWidget)
+        self.mainLayout.addWidget(self.questionWidget)
+        self.mainLayout.addWidget(self.answerWidget)
+        # noinspection PyUnresolvedReferences
         self.question_confirm.clicked.connect(self.check_answer)
         self.resize(500, 200)
 
         self.answer_buttons = [QRadioButton() for _ in range(3)]
         for button in self.answer_buttons:
-            self.answers.addWidget(button)
+            self.choicesLayout.addWidget(button)
 
-        self.drugs = dict(drugs)
-        self.groups = list(groups)
+        self.answer_idx = None
+        
+        self.question = None
+        self.answer = None
+
+        self.drug = None
+        self.group = None
 
         self.previous = 'drug'
         self.ask_question()
@@ -96,76 +100,43 @@ class App(QMainWindow):
         self.answerWidget.hide()
         self.questionWidget.show()
 
-        group_answers = list(self.drugs[self.group].values())
-        options = list(range(3))
-        random.shuffle(options)
-        self.answer_text = self.drugs[self.group][self.drug]
+        self.question = self.drug
+        self.answer = drugs[self.group][self.drug]
 
-        self.answer = options.pop()
+        choices = self.generate_choices(drugs[self.group].values())
 
-        wrong_answers = list(group_answers)
-        wrong_answers.remove(self.drugs[self.group][self.drug])
-        random.shuffle(wrong_answers)
+        self.questionLabel.setText('What is {}?'.format(self.drug))
 
-        possible_answers: list = [None] * 3
-
-        possible_answers[self.answer] = self.drugs[self.group][self.drug]
-
-        if len(wrong_answers) > 0:
-            possible_answers[options.pop()] = wrong_answers.pop()
-        else:
-            possible_answers[options.pop()] = ''
-
-        self.question.setText('What is {}?'.format(self.drug))
-
-        for i, possible_answer in enumerate(possible_answers):
-            self.answer_buttons[i].setText(possible_answers[i])
+        for i, possible_answer in enumerate(choices):
+            self.answer_buttons[i].setText(choices[i])
 
         self.previous = 'drug'
 
         # self.resize(self.Layout.sizeHint())
 
     def question_group(self):
-        if sum([len(self.drugs[group]) for group in self.groups]) == 0:
+        if sum([len(drugs[group]) for group in groups]) == 0:
 
             self.result.setText('Well done, you completed all the drugs!')
-            self.questionWidget.hide()
+            self.questionLabel.hide()
             self.answerWidget.show()
             self.answer_screen_confirm.hide()
             return
-
 
         self.answerWidget.hide()
         self.questionWidget.show()
 
         self.update_answer()
-        options = list(range(3))
-        random.shuffle(options)
 
-        self.answer = options.pop()
+        self.question = self.drug
+        self.answer = self.group
 
-        self.answer_text = self.group
+        choices = self.generate_choices(groups)
 
-        wrong_answers = list(self.groups)
-        wrong_answers.remove(self.group)
-        random.shuffle(wrong_answers)
+        self.questionLabel.setText('What group is {} in?'.format(self.drug))
 
-        possible_answers: list = [None] * 3
-
-        possible_answers[self.answer] = self.group
-        if len(wrong_answers) > 0:
-            possible_answers[options.pop()] = wrong_answers.pop()
-        else:
-            possible_answers[options.pop()] = ''
-        if len(wrong_answers) > 0:
-            possible_answers[options.pop()] = wrong_answers.pop()
-        else:
-            possible_answers[options.pop()] = ''
-
-        self.question.setText('What group is {} in?'.format(self.drug))
-
-        for i, possible_answer in enumerate(possible_answers):
-            self.answer_buttons[i].setText(possible_answers[i])
+        for i, possible_answer in enumerate(choices):
+            self.answer_buttons[i].setText(choices[i])
 
         self.previous = 'group'
 
@@ -174,62 +145,43 @@ class App(QMainWindow):
         self.questionWidget.show()
 
         self.update_answer()
-        options = list(range(3))
-        random.shuffle(options)
-        self.answer_text = self.drug
-        self.questionText = self.drugs[self.group][self.drug]
 
-        self.answer = options.pop()
-        group_answers = list(self.drugs[self.group].keys())
+        self.question = drugs[self.group][self.drug]
+        self.answer = self.drug
 
-        wrong_answers = list(group_answers)
-        wrong_answers.remove(self.drug)
-        random.shuffle(wrong_answers)
+        choices = self.generate_choices(drugs[self.group].keys())
 
-        possible_answers: list = [None] * 3
+        self.questionLabel.setText(self.question)
 
-        possible_answers[self.answer] = self.drug
-        if len(wrong_answers) > 0:
-            possible_answers[options.pop()] = wrong_answers.pop()
-        else:
-            possible_answers[options.pop()] = ''
-        if len(wrong_answers) > 0:
-            possible_answers[options.pop()] = wrong_answers.pop()
-        else:
-            possible_answers[options.pop()] = ''
-
-        self.question.setText('What is {}?'.format(self.drugs[self.group][self.drug]))
-
-        for i, possible_answer in enumerate(possible_answers):
-            self.answer_buttons[i].setText(possible_answers[i])
+        for i, possible_answer in enumerate(choices):
+            self.answer_buttons[i].setText(choices[i])
 
         self.previous = 'description'
 
     def update_answer(self):
-        self.group = random.choice(self.groups)
-        group_drugs = list(self.drugs[self.group].keys())
+        self.group = random.choice(groups)
+        group_drugs = list(drugs[self.group].keys())
         self.drug = random.choice(group_drugs)
-        self.questionText = self.drug
 
     def check_answer(self):
         for i, a in enumerate(self.answer_buttons):
 
             if a.isChecked():
-                if i == self.answer:
-                    self.result.setText('Correct! \n\n{} is {}'.format(self.questionText, self.answer_text))
+                if i == self.answer_idx:
+                    self.result.setText('Correct! \n\n{} is {}'.format(self.question, self.answer))
                     self.answerPicture.setPixmap(QPixmap(
                         random.choice([os.path.join('images', image) for image in os.listdir('images')
                                        if 'chicken' in image])).scaledToHeight(200))
                     self.total_correct += 1
 
                     if self.previous != 'group':
-                        self.drugs[self.group].pop(self.drug)
+                        drugs[self.group].pop(self.drug)
                         self.progressWidget.setValue(self.progressWidget.value() + 1)
 
-                    if len(self.drugs[self.group]) == 0:
-                        self.groups.remove(self.group)
+                    if len(drugs[self.group]) == 0:
+                        groups.remove(self.group)
                 else:
-                    self.result.setText('Wrong!\n\n{} is actually {}'.format(self.questionText, self.answer_text))
+                    self.result.setText('Wrong!\n\n{} is actually {}'.format(self.question, self.answer))
                     self.answerPicture.setPixmap(QPixmap(
                         random.choice([os.path.join('images', image) for image in os.listdir('images')
                                        if 'rooster' in image])
@@ -241,10 +193,33 @@ class App(QMainWindow):
         self.total_answered += 1
 
         self.scoreWidget.setText('{} out of {} correct so far, {} drugs remaining'.format(
-            self.total_correct, self.total_answered, sum([len(self.drugs[group]) for group in self.groups])))
+            self.total_correct, self.total_answered, sum([len(drugs[group]) for group in groups])))
         self.questionWidget.hide()
         self.answerWidget.show()
         # self.resize(self.questionLayout.sizeHint())
+
+    def generate_choices(self, possible_answers):
+        options = list(range(3))
+        random.shuffle(options)
+
+        self.answer_idx = options.pop()
+        self.answer_buttons[self.answer_idx].show()
+
+        wrong_answers = list(possible_answers)
+        wrong_answers.remove(self.answer)
+        random.shuffle(wrong_answers)
+
+        choices: list = [None] * 3
+
+        choices[self.answer_idx] = self.answer
+        for idx in options:
+            if len(wrong_answers) > 0:
+                choices[idx] = wrong_answers.pop()
+                self.answer_buttons[idx].show()
+            else:
+                self.answer_buttons[idx].hide()
+
+        return choices
 
 
 if __name__ == '__main__':
